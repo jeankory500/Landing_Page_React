848e72e6a3f91316d5f948b8f01f7458176c1e5c
jeankory500 <jeankory123@gmail.com>
2023-05-16 00:40:43 +0000

    firstSave

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,9 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Navbar from "./component/Navbar.jsx";
+import Jumbotron from "./component/Jumbotron.jsx";
+import CardSec from "./component/cardSec.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<> <Navbar/><Jumbotron/><CardSec/><Home/></>, document.querySelector("#app"));
